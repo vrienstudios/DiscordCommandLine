60863cd689d115267884b916024cd8766f90e64b
Chay8 <visionistprojects@gmail.com>
2020-01-01 23:57:19 -0500

    fixed file directory

---
+++ DSC/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSC
+{
+    public static class StaticData
+    {
+        public static Data.User CurrentUser { get; set; }
+        public static List<Data.Guild> Guilds { get; set; }
+    }
+}
