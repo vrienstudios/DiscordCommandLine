17d7ce44b777ec93a385fccc2657678acdb5c3de
Chay <visionistprojects@gmail.com>
2020-01-09 15:11:29 -0500

    Update Data.cs

--- DSC/Data.cs
+++ DSC/Data.cs
@@ -10,5 +10,6 @@ namespace DSC
     {
         public static Data.User CurrentUser { get; set; }
         public static List<Data.Guild> Guilds { get; set; }
+        public static List<Data.EventTypes.MESSAGE_CREATE.Event_message_create> Messages {get; set;}
     }
 }
